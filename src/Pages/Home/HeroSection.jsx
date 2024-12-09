a2b8747a873edb31c0f0feb13fd5607ed6b96497
HaniBassam <143654847+HaniBassam@users.noreply.github.com>
2024-12-09 12:52:45 +0100

    responsive

--- src/Pages/Home/HeroSection.jsx
+++ src/Pages/Home/HeroSection.jsx
@@ -1,11 +1,16 @@
 import { Typewriter } from "react-simple-typewriter";
 
+export const data = {
+  
+}
+
+
 export default function HeroSection() {
   return (
     <section id="heroSection" className="hero--section">
       <div className="hero--section--content--box">
+        <p className="section--title">Hey, I'm Hani</p>
         <div className="hero--section--content">
-          <p className="section--title">Hey, I'm Hani</p>
           <h1 className="hero--section--title">
             <span className="hero--section-title--color"></span> <br />
             <Typewriter
@@ -19,8 +24,9 @@ export default function HeroSection() {
             />
           </h1>
           <p className="hero--section-description1">
-            I create modern,<br /> user-friendly designs and <br /> innovative solutions for
-            web <br />  and digital media.
+            I create modern,
+            <br /> user-friendly designs and <br /> innovative solutions for web{" "}
+            <br /> and digital media.
           </p>
           <button className="btn btn-primary">Get In Touch</button>
         </div>
